0251e38a3e991bbedbf7468112ccbccf10b56452
Anran-code <489459903@qq.com>
2025-12-14 15:13:08 +0800

    update menu

--- StardewValley/Classes/Menu.cpp
+++ StardewValley/Classes/Menu.cpp
@@ -18,11 +18,60 @@ bool MenuScene::init()
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
+    // 1.±³¾°
+    auto bgsprite = Sprite::create("ui/menu_bg.png");
+    Size targrtSize = visibleSize;
+    Size imgSize = bgsprite->getContentSize();
+    float scaleX = targrtSize.width / imgSize.width;
+    float scaleY = targrtSize.height / imgSize.height;
+    float scale = std::min(scaleX, scaleY);
+    bgsprite->setScale(scale);
+    // position the sprite on the center of the screen
+    bgsprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
 
+    // add the sprite as a child to this layer
+    this->addChild(bgsprite);
+
+    // 2.±êÌâ
+    auto ttsprite = Sprite::create("ui/menu_logo.png");
+    ttsprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height*0.75 + origin.y));
+
+    // add the sprite as a child to this layer
+    this->addChild(ttsprite);
+
+    auto closeItem = MenuItemImage::create(
+        "ui/btn_exit.png",
+        "ui/btn_exit_close.png",
+        CC_CALLBACK_1(MenuScene::menuCloseCallback, this));
+    float x1 = origin.x + 0.75 * visibleSize.width - closeItem->getContentSize().width / 2;
+    float y1 = origin.y + 0.15 * visibleSize.height + closeItem->getContentSize().height / 2;
+    closeItem->setPosition(Vec2(x1, y1));
+    closeItem->setScale(2.0);
+
+    auto newItem = MenuItemImage::create(
+        "ui/btn_new.png",
+        "ui/btn_new_close.png",
+        CC_CALLBACK_1(MenuScene::menuNewCallback, this));
+    float x2 = origin.x + 0.25 * visibleSize.width - newItem->getContentSize().width / 2;
+    float y2 = origin.y + 0.15 * visibleSize.height + newItem->getContentSize().height / 2;
+    newItem->setPosition(Vec2(x2, y2));
+    newItem->setScale(2.0);
+
+    auto menu1 = Menu::create(closeItem, NULL);
+    menu1->setPosition(Vec2::ZERO);
+    this->addChild(menu1);
+    auto menu2 = Menu::create(newItem, NULL);
+    menu2->setPosition(Vec2::ZERO);
+    this->addChild(menu2);
 }
 
 
 void MenuScene::menuCloseCallback(Ref* pSender)
+{
+    Director::getInstance()->end();
+}
+
+void MenuScene::menuNewCallback(Ref* pSender)
 {
     Director::getInstance()->end();
 }
--- StardewValley/Classes/Menu.h
+++ StardewValley/Classes/Menu.h
@@ -5,12 +5,15 @@
 
 class MenuScene : public cocos2d::Scene
 {
+public:
     static cocos2d::Scene* createScene();
 
     virtual bool init();
 
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
+
+    void menuNewCallback(cocos2d::Ref* pSender);
 
     // implement the "static create()" method manually
     CREATE_FUNC(MenuScene);
